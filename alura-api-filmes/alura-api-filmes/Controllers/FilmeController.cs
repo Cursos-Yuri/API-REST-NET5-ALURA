e3f57b9c2072ec4cfea0348154fc2dd054195c44
yurimoyses09 <yuri09.moyses@gmail.com>
2022-08-22 23:21:29 -0300

    Criando projeto

---
+++ alura-api-filmes/alura-api-filmes/Controllers/FilmeController.cs
@@ -0,0 +1,60 @@
+﻿using alura_api_filmes.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace alura_api_filmes.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        public FilmeController()
+        {
+
+        }
+
+        [HttpPost]
+        public IActionResult CreateFilme([FromBody] Filme filme) 
+        {
+            try
+            {
+                filmes.Add(filme);
+
+                return CreatedAtAction(nameof(GetFilmePerId), new { Id = filme.Id }, filme);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { ex.Message });
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetFilmes() 
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { ex.Message });
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetFilmePerId(int id)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { ex.Message });
+            }
+        }
+    }
+}
---
+++ alura-api-filmes/alura-api-filmes/Data/FilmeContext.cs
@@ -0,0 +1,15 @@
+﻿using alura_api_filmes.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace alura_api_filmes.Data
+{
+    public class FilmeContext : DbContext
+    {
+        public FilmeContext(DbContextOptions<FilmeContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<Filme> Filme { get; set; }
+    }
+}
